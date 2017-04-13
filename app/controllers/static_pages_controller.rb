6e77cb6a4cff7f2bb97bc158343c1a4d1a88b298
wjfletcher <warrenjfletcher@gmail.com>
2017-04-13 16:45:29 -0400

    contact modal

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,10 @@
 class StaticPagesController < ApplicationController
 
-  
+  def create
+    binding.pry
+    name = params[:name]
+    email = params[:email]
+    message = params[:message]
+    ContactMailer.contact_email(name, email, message).deliver
+  end
 end
---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,8 @@
+class ContactMailer < ApplicationMailer
+  def contact_email(name, email, message)
+    @name = name
+    @email = email
+    @message = message
+    mail(from: @email, to: "warren@warrenjfletcher.tech", subject:"Message from warrenjfletcher.tech")
+  end
+end
